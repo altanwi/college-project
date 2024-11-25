912f01bb7c2af92f2b8744109aee8f9a63e2e978
altanwi <aaan22ece@cmrit.ac.in>
2024-11-25 22:39:01 +0530

    script.js

--- script.js
+++ script.js
@@ -1,98 +1,75 @@
-const calculator = {
-    displayValue: '0',
-    firstOperand: null,
-    waitingForSecondOperand: false,
-    operator: null,
-};
 
-function inputDigit(digit) {
-    const { displayValue, waitingForSecondOperand } = calculator;
 
-    if (waitingForSecondOperand === true) {
-        calculator.displayValue = digit;
-        calculator.waitingForSecondOperand = false;
-    } else {
-        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-    }
+function load() {
+  var btns = document.querySelectorAll("#calculator span");
+  var operators = ["+", "-", "x", "÷"];
+  var inputScreen = document.querySelector("#screen");
+  var btnValue;
+  var input;
+
+  for (var i = 0; i < btns.length; i++) {
+    var decimalAdded = false; // Flag used to avoid two decimal
+
+    btns[i].addEventListener("click", function() {
+      btnValue = this.innerHTML;
+      input = inputScreen.innerHTML;
+
+      switch (btnValue) {
+        case "C":
+          inputScreen.innerHTML = "";
+          decimalAdded = false;
+          break;
+        case "=":
+          // Last char of string
+          var lastChar = input[input.length - 1];
+
+          // Replace x to *, + to / which could be calculated in eval
+          input = input.replace(/x/g, "*").replace(/÷/g, "/");
+
+          // Checking the last character of the input.
+          // If it's an operator or a decimal, remove it
+          // /.$/ means last char in regex
+          if (operators.indexOf(lastChar) > -1 || lastChar == ".")
+            input = input.replace(/.$/, "");
+
+          if (input) {
+            // If the argument is an expression, eval() evaluates the expression.
+            // If the argument is one or more JavaScript statements, eval() executes the statements.
+            inputScreen.innerHTML = eval(input);
+          }
+          decimalAdded = false;
+          break;
+        case ".":
+          if (!decimalAdded) {
+            inputScreen.innerHTML += btnValue;
+            decimalAdded = true;
+          }
+          break;
+        case "+":
+        case "-":
+        case "x":
+        case "÷":
+          // Last char of string
+          var lastChar = input[input.length - 1];
+
+          // Only add operator if input is not empty and there is no operator at the last
+          if (input != "" && operators.indexOf(lastChar) == -1)
+            inputScreen.innerHTML += btnValue;
+          // Allows minus if the string is empty. The first number could be under zero
+          else if (input == "" && btnValue == "-")
+            inputScreen.innerHTML += btnValue;
+
+          // Allows to represent the last operation
+          if (operators.indexOf(lastChar) > -1 && input.length > 1) {
+            inputScreen.innerHTML = input.replace(/.$/, btnValue);
+          }
+          decimalAdded = false;
+          break;
+        default:
+          inputScreen.innerHTML += btnValue;
+          decimalAdded = false;
+          break;
+      }
+    });
+  }
 }
-
-function inputDecimal(dot) {
-    if (calculator.waitingForSecondOperand === true) return;
-
-    if (!calculator.displayValue.includes(dot)) {
-        calculator.displayValue += dot;
-    }
-}
-
-function handleOperator(nextOperator) {
-    const { firstOperand, displayValue, operator } = calculator;
-    const inputValue = parseFloat(displayValue);
-
-    if (operator && calculator.waitingForSecondOperand)  {
-        calculator.operator = nextOperator;
-        return;
-    }
-
-    if (firstOperand == null && !isNaN(inputValue)) {
-        calculator.firstOperand = inputValue;
-    } else if (operator) {
-        const result = performCalculation[operator](firstOperand, inputValue);
-
-        calculator.displayValue = String(result);
-        calculator.firstOperand = result;
-    }
-
-    calculator.waitingForSecondOperand = true;
-    calculator.operator = nextOperator;
-}
-
-const performCalculation = {
-    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
-    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
-    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
-    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
-    '=': (firstOperand, secondOperand) => secondOperand
-};
-
-function resetCalculator() {
-    calculator.displayValue = '0';
-    calculator.firstOperand = null;
-    calculator.waitingForSecondOperand = false;
-    calculator.operator = null;
-}
-
-function updateDisplay() {
-    const display = document.querySelector('.calculator-screen');
-    display.value = calculator.displayValue;
-}
-
-updateDisplay();
-
-const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) => {
-    const { target } = event;
-    if (!target.matches('button')) {
-        return;
-    }
-
-    if (target.classList.contains('operator')) {
-        handleOperator(target.value);
-        updateDisplay();
-        return;
-    }
-
-    if (target.classList.contains('decimal')) {
-        inputDecimal(target.value);
-        updateDisplay();
-        return;
-    }
-
-    if (target.classList.contains('all-clear')) {
-        resetCalculator();
-        updateDisplay();
-        return;
-    }
-
-    inputDigit(target.value);
-    updateDisplay();
-});
